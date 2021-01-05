355790efd00fbac3cededfd639f92bcc259fe6a5
AlyMBarakat <aly.m.barakat@gmail.com>
2021-01-05 22:28:23 +0300

    Change pot to enironment variable

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 */
 const aedes = require('aedes')()
 const server = require('net').createServer(aedes.handle)
-const port = 8080
+const port = process.env.PORT || 8080;
 
 server.listen(port, function () {
     console.log('server started and listening on port ', port);
